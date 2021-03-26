d7bbb4cecdcd6f0ccfcfff74f8cbd679f4b5f768
Dydy2412 <Dydy2412@users.noreply.github.com>
2021-03-26 18:15:17 +1100

    Tout remis comme avant les tests

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-##--Dydy2412--& GalTech mais pas trop nn plus##
+##--Dydy2412--##
 from lib.labygen_unp import gen_laby_unp
 from lib.labygen_exr import gen_laby_ex
 from lib.labyprinter import print_to_png
